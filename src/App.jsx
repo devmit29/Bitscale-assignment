d88bb87bb9bcc631a73fe4aef0ba680c903dc55d
Dev Mittal <mittal.dev2002@gmail.com>
2025-01-14 14:09:48 +0530

    check svg 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import Chart from './Chart';
+import arrowLeft from './assets/arrow-left.svg';
+import userIcon from './assets/user-03.svg';
+import tableIcon from './assets/table.svg';
+import puzzleIcon from './assets/puzzle-piece-01.svg';
+import circleIcon from './assets/intersect-circle.svg';
+import creditCardIcon from './assets/credit-card-02.svg';
+import coinsIcon from './assets/coins-stacked-01.svg';
 
 function App() {
   const [autoSave, setAutoSave] = useState(false);
@@ -14,7 +21,7 @@ function App() {
     <>
       <nav className='border-b-2 border-gray-150 w-full flex justify-between items-center text-white p-4 '>
         <div className='flex justify-center items-center'>
-          <img src='public/assets/arrow-left.svg' alt='back-arrow' className='mr-2 cursor-pointer' />
+          <img src={arrowLeft} alt='back-arrow' className='mr-2 cursor-pointer' />
           <p className='text-gray-500'>Name of the file</p>
         </div>
         <div className='flex justify-between items-center'>
@@ -32,26 +39,26 @@ function App() {
             <p className={`ml-2 font-medium text-sm ${autoSave ? 'text-green-500' : 'text-gray-500'}`}>Auto Save</p>
           </div>
           <div className='flex justify-center items-center bg-[#FEECDC] ml-2 p-2 rounded-full'>
-            <img src='src/assets/user-03.svg' alt='user' className='cursor-pointer' />
+            <img src={userIcon} alt='user' className='cursor-pointer' />
           </div>
         </div>
       </nav>
       <div className='flex h-full w-full'>
         <div className='border-r-2 h-full border-gray-150 w-16 flex flex-col justify-between items-center p-4'>
-        <div className='flex flex-col items-center'>
-            <img className='my-4 cursor-pointer' width="24px" height={'24px'} src="src/assets/table.svg" alt="table-icon" />
-            <img className='my-4 cursor-pointer' width="24px" height={'24px'} src="src/assets/puzzle-piece-01.svg" alt="puzzle-icon" />
-            <img className='my-4 cursor-pointer' width="24px" height={'24px'} src="src/assets/intersect-circle.svg" alt="circle-icon" />
-        </div>
-        <div className='flex justify-end flex-col items-center'>
-            <img className='my-4 cursor-pointer' width="24px" height={'24px'} src="src/assets/credit-card-02.svg" alt="credit-card-icon" />
-            <img className='my-4 cursor-pointer' width="24px" height={'24px'} src="src/assets/coins-stacked-01.svg" alt="coins-icon" />
-        </div>
+          <div className='flex flex-col items-center'>
+            <img className='my-4 cursor-pointer' width="24px" height={'24px'} src={tableIcon} alt="table-icon" />
+            <img className='my-4 cursor-pointer' width="24px" height={'24px'} src={puzzleIcon} alt="puzzle-icon" />
+            <img className='my-4 cursor-pointer' width="24px" height={'24px'} src={circleIcon} alt="circle-icon" />
+          </div>
+          <div className='flex justify-end flex-col items-center'>
+            <img className='my-4 cursor-pointer' width="24px" height={'24px'} src={creditCardIcon} alt="credit-card-icon" />
+            <img className='my-4 cursor-pointer' width="24px" height={'24px'} src={coinsIcon} alt="coins-icon" />
+          </div>
         </div>
         <Chart />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
